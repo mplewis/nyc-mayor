9c676adb36a288531893919901a55a7e0a567b95
Matt Lewis <matt@mplewis.com>
2024-10-02 23:57:37 -0600

    feat: progress bar with indicator

--- scenes/ui/progress.gd
+++ scenes/ui/progress.gd
@@ -8,6 +8,7 @@ extends Control
 
 @onready var outer: NinePatchRect = $Outer
 @onready var inner: TextureRect = $Inner
+@onready var pointer: TextureRect = $Pointer
 
 
 func convert_scale(
@@ -20,5 +21,8 @@ func _process(_delta):
 	var p = clamp(progress, 0, 1)
 	var total_width := outer.size.x
 	p = convert_scale(p, 0, 1, margin_left_px, total_width - margin_right_px) / total_width
+
 	inner.texture.region.size.x = p * total_width
 	inner.scale.x = p
+
+	pointer.position.x = inner.position.x + inner.texture.region.size.x - pointer.size.x / 2
